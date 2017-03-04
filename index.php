cea14479a28505c61c304b1c09f554ab66e3fea5
evgShes <mr.shes.mail@gmail.com>
2017-03-04 22:06:03 +0300

    initial commit

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ $file_path = $log_path . '\\' . $file_name;
 $file = file($file_path);
 $val = preg_split('/[?^\s]+/', $file[200]);
 echo '<pre>';
-//print_r($val);
+//print_r($val);sss
 var_dump(date('d.m.Y H:i:s', $val[0]), $val[0], strtotime(date('d.m.Y H:i:s', $val[0])));
 echo '</pre>';
