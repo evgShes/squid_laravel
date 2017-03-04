6dd374ea40b2a303ad49ace7a2e101c7382b4cdc
evgShes <mr.shes.mail@gmail.com>
2017-03-04 21:50:08 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$log_path = 'C:\squid\var\logs';
+$file_name = 'access.log';
+$file_path = $log_path . '\\' . $file_name;
+$file = file($file_path);
+$val = preg_split('/[?^\s]+/', $file[200]);
+echo '<pre>';
+//print_r($val);
+var_dump(date('d.m.Y H:i:s', $val[0]), $val[0], strtotime(date('d.m.Y H:i:s', $val[0])));
+echo '</pre>';
