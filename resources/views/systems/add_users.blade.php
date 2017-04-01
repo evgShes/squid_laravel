b3a78d9e8dbf130efdab57da87d85d20fd6a6e43
1kris1 <crist.fedencko2009@yandex.ru>
2017-04-01 19:49:53 +0300

    отправка формы

--- resources/views/systems/add_users.blade.php
+++ resources/views/systems/add_users.blade.php
@@ -49,7 +49,7 @@
 
     <div class="row">
         <div class="col offset-m10 m2 input-field  ">
-            <button class="btn waves-light green waves-effect" type="button" id="add_users">Добавить</button>
+            <button class="btn waves-light green waves-effect" type="button" id="add_users" data-url="#">Добавить</button>
         </div>
     </div>
 
