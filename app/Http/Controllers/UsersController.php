1479389ec158cf13253dee79921c534c9a22a45c
evgShes <mr.shes.mail@gmail.com>
2017-04-12 12:24:42 +0300

    test commit

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-
+    /// test
     public function saveEmployer(Request $request){
         $save_user = UsersList::trtCreateOrEdit($request->all());
         dd($save_user);
