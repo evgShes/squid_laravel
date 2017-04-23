b88643db0ad5bf10f769f2f0ae2317f6938cf8a8
evgShes <mr.shes.mail@gmail.com>
2017-04-23 22:06:40 +0300

    вывод логов apache

--- config/apache.php
+++ config/apache.php
@@ -3,5 +3,5 @@ return [
     'regex' => '/^([^\s:]+)\s([\S]+)\s\[([^\[\]]+)]\s([\S]+)\s([\S]+\s[\S]+)\s([\S]+)\s"([^"]+)"\s"([^"]+)"\s([\d|-]+)\s([\d|-]+)\s([\d|-]+)\s([\d|-]+)\s([\d|-]+)/',
     'path_apache_log' => 'D:\OpenServer\userdata\logs',
     'name_apache_log' => 'Apache-2.4-x64_queriesa.log',
-    'LogFormat' => '%v: %a %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %I %O %B',
+    'LogFormat' => '"%v %a %t %r %>s \"%{Referer}i\" \"%{User-Agent}i\" %b %I %O %B %D"',
 ];
